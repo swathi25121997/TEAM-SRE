4c7a605dfcfc7ad167ce24d14d7763c7f334f1bf
swathi25121997 <104359251+swathi25121997@users.noreply.github.com>
2022-05-10 11:56:00 +0530

    tf 1

---
+++ main.tf
@@ -0,0 +1,15 @@
+
+provider "aws" {
+    region     = "ap-south-1"
+    access_key = "AKIA24LGQVQK35WAWAN4"
+    secret_key = "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"
+
+}
+
+provider "github" {
+    token = "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"
+  
+}
+
+
+
