ac8a748bc671a0447172e224bba067a90614939e
swathi25121997 <104359251+swathi25121997@users.noreply.github.com>
2022-05-13 13:31:10 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,13 +1,13 @@
 
 provider "aws" {
     region     = "ap-south-1"
-    access_key = "AKIA24LGQVQK35WAWAN4"
-    secret_key = "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"
+    access_key = "XXXXXXXXXXX"
+    secret_key = "XXXXXXXXXXX"
 
 }
 
 provider "github" {
-    token = "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"
+    token = "XXXXXXXXXXXX"
   
 }
 
